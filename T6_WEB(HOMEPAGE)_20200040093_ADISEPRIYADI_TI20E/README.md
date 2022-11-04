b7fe564771d948beb6271990d8a8767720d306cc
Adiissep <74800424+Adiissep@users.noreply.github.com>
2022-11-04 15:35:54 +0700

    Add files via upload

---
+++ T6_WEB(HOMEPAGE)_20200040093_ADISEPRIYADI_TI20E/README.md
@@ -0,0 +1,4 @@
+# mangcoding-homepage
+NIM : 20200040093   
+Nama : Adi Sepriyadi
+Kelas : TI20E
---
+++ T6_WEB(HOMEPAGE)_20200040093_ADISEPRIYADI_TI20E/popup.js
@@ -0,0 +1,3 @@
+document.querySelector(".start").addEventListener('click', function(){
+    Swal.fire("Our First Alert", "With some body text and success icon!", "success");
+  });
